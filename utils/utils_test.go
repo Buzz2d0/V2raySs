a4bf86e6c56dfb74cd2cbacc1dd49a075f1c16e2
whoami <admin@imipy.com>
2020-09-14 09:20:59 +0800

    build

--- utils/utils_test.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "testing"
-
-func TestCheckAppTmp(t *testing.T) {
-	t.Log(CheckAppTmp(".sub"))
-}
--- vmess/hosts_test.go
+++
@@ -1,9 +0,0 @@
-package vmess
-
-import "testing"
-
-func TestParseSubscription(t *testing.T) {
-	subURL := "https://sub/link/"
-	t.Log(subURL)
-	t.Log(ParseSubscription(subURL))
-}
